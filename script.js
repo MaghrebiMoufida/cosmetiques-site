ef9d991088e4ccb9a1947801303828e431e09388
MaghrebiMoufida <151971073+MaghrebiMoufida@users.noreply.github.com>
2024-03-05 11:20:54 +0100

    ajout d'une nouvelle modification

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// scripts.js
-
-// Lorsque le bouton "Shop Now" est cliqué, cela fait défiler l'écran vers la section "Products"
 document.querySelector('.cta-button').addEventListener('click', function() {
     document.querySelector('#products').scrollIntoView({ behavior: 'smooth' });
   });
   
-  // Lorsque l'un des boutons "Learn More" est cliqué, cela affiche une alerte avec la description du produit correspondant
   document.querySelectorAll('.learn-more-button').forEach(function(button) {
     button.addEventListener('click', function() {
       const description = this.previousElementSibling.innerText;
@@ -13,7 +9,6 @@ document.querySelector('.cta-button').addEventListener('click', function() {
     });
   });
   
-  // Lorsque le formulaire de contact est soumis, cela affiche un message de confirmation
   document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Merci pour votre message ! Nous vous répondrons dans les plus brefs délais.');
